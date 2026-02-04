633402dc968527be198c9fab48a9356c33d641b3
Federico Monfasani <67231410+fmonfasani@users.noreply.github.com>
2026-02-04 16:03:02 -0300

    Add transformIgnorePatterns to jest config

--- jest.config.js
+++ jest.config.js
@@ -13,6 +13,7 @@ const customJestConfig = {
   moduleNameMapper: {
     "^@/(.*)$": "<rootDir>/src/$1",
   },
+  transformIgnorePatterns: ["/node_modules/(?!next-intl|use-intl)/"],
   testPathIgnorePatterns: ["/node_modules/", "/.next/", "/e2e/"],
   collectCoverageFrom: [
     "src/**/*.{js,jsx,ts,tsx}",
